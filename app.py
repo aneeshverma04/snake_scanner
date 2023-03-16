35c87e47796820fa3e6653b0dfef71381e10f06a
aneeshverma04 <aneesh.verma09@gmail.com>
2023-03-17 03:19:00 +0530

    Development, cookie extraction, encryption, decryption finished.

--- app.py
+++ app.py
@@ -3,9 +3,19 @@ import masscan
 import nmap3
 import json
 import ast
+import requests
+import base64
+import pickle 
+import pickletools
+from io import StringIO
 
 def nmap_python_ports_fetch(nmap_result,ip):
-    formatted_result = nmap_result[ip]['ports']
+    if ip in nmap_result:
+        formatted_result = nmap_result[ip]['ports']
+    else:
+        print("Nmap Result incomplete")
+        return []
+
     size = len(formatted_result)
     list_ports = []
 
@@ -37,14 +47,18 @@ def masscan_ports_fetch(speed,ip):
     mscan.scan(ip, ports='1-65535', arguments='--max-rate '+ str(speed))
     
     mscan_dict = ast.literal_eval(mscan.scan_result)
-    mscan_result_formatted = mscan_dict["scan"][ip]
+    # print(mscan_dict)
+    if ip in mscan_dict["scan"]:
+        mscan_result_formatted = mscan_dict["scan"][ip]
+    else:
+        return []
 
     size_mscan = len(mscan_result_formatted)
     list_mscan_ports = []
 
     for i in range(size_mscan):
         list_mscan_ports.append(mscan_result_formatted[i]['port'])
-
+    
     print(list_mscan_ports)
 
     return list_mscan_ports
@@ -59,19 +73,25 @@ def check_website_python(url):
     ip = subprocess.check_output(command, shell=True).rstrip().decode('utf-8')
     print(ip)
     # Use masscan and search for all possible open ports (tcp)
-    # mscan_list_1000 = masscan_ports_fetch(1000,ip)
-    # mscan_list_500 = masscan_ports_fetch(500,ip)
-    # mscan_list_250 = masscan_ports_fetch(250,ip)
-    # # mscan_list_100 = masscan_ports_fetch(100,ip)
-    
-    # # mscan_list = mscan_list_1000 + mscan_list_500 + mscan_list_250 + mscan_list_100
-    # mscan_list = mscan_list_1000 + mscan_list_500 + mscan_list_250
-    # mscan_list_unique = list(set(mscan_list))
+    mscan_list_1000 = masscan_ports_fetch(1000,ip)
+    mscan_list_500 = masscan_ports_fetch(500,ip)
+    mscan_list_250 = masscan_ports_fetch(250,ip)
+    # mscan_list_100 = masscan_ports_fetch(100,ip)
+
+    mscan_list = mscan_list_1000 + mscan_list_500 + mscan_list_250
+    # mscan_list = mscan_list_1000 + mscan_list_500 + mscan_list_250 + mscan_list_100
+
+    # mscan_list = []
+    # for i in range(5):
+    #     mscan_list = mscan_list + masscan_ports_fetch(1000,ip)
+   
+
+    mscan_list_unique = list(set(mscan_list))
+    # mscan_list_unique = [80,443]
     
     # print(mscan_list_unique)
     
-    #mscan_list_unique = [30477, 32272, 32157, 31522, 30119, 32171, 30125, 32558, 31795, 32309, 32186, 30654, 30532, 31948, 30415, 30927, 32350, 30432, 31074, 46311, 31841]
-    mscan_list_unique = [31415]    
+    # mscan_list_unique = [30341,32045]    
     mscan_list_str = ''.join(str(val)+"," for val in mscan_list_unique)
     mscan_list_str = mscan_list_str[:-1]    
     print(mscan_list_str)
@@ -85,17 +105,79 @@ def check_website_python(url):
     # nmap = nmap3.NmapScanTechniques()
     # nmap_result = nmap.nmap_tcp_scan(url)
     # print(json.dumps(nmap_result, indent=4))
+    # print(nmap_result)
 
     python_ports = nmap_python_ports_fetch(nmap_result,ip)
     ports_str = ''.join(str(val)+"," for val in python_ports)
     ports_str = ports_str[:-1]  
+    if ports_str == '':
+        print ("There are no ports containing python server")
+        return []
     print("The ports which contain a python server are:"+ports_str)
     return python_ports
+    #return mscan_list_unique
     
-def fetch_editable_data(ports):
-    pass
+def fetch_editable_data(url,ports):
+    editable_data = []
+    
+    for port in ports:
+        new_session = requests.session()
+        if port == 443:
+            url_complete = "https://"+url+":"+str(port)
+            #resp = new_session.get("https://"+url+str(port))
+            new_session.get(url_complete)
+        else:
+            url_complete = "http://"+url+":"+str(port)
+            #resp = new_session.get("http://"+url+":"+str(port))
+            new_session.get(url_complete)
+        cookie = list(new_session.cookies.get_dict().values())
+        if cookie == []:
+            cookie = ['None']
+        editable_data = editable_data + cookie
+
+    return (editable_data)
+
+        # start fuzzing and get cookies of each
+        # wordlist_file = open("common.txt","r")
+
+        # for l in wordlist_file:
+        #     w = l.strip()
+        #     url_new = url_complete + "/" + w
+        #     resp = new_session.get(url_new)
+        #     if resp:
+        #         print("Discovered directory at this link: " + url_new)
+        #         print (new_session.cookies.get_dict()) 
+
+def decrypting_cookies(data):
+    decrypted_data = []
+    for val in data:
+        # if val is base 64
+        decrypted_data.append(base64.b64decode(val))
+    return decrypted_data
+
+def check_object_picklable(data):
+    picklable_data = []
+    for val in data:
+        try:
+            pickletools.dis(val,out=StringIO())
+            picklable_data.append(True)
+        except:
+            picklable_data.append(False)
+    StringIO().close
+    return picklable_data
 
 if __name__=="__main__":
     url = input("Enter the website you want to scan: ")
     python_ports = check_website_python(url)
-    fetch_editable_data(python_ports)
+    encrypted_data = fetch_editable_data(url,python_ports)
+    decrypted_data = decrypting_cookies(encrypted_data)
+    # decrypted_data = [b"(dp0\nS'serum'\np1\nccopy_reg\n_reconstructor\np2\n(c__main__\nanti_pickle_serum\np3\nc__builtin__\nobject\np4\nNtp5\nRp6\ns.","dadas",b"(dp0\nS'serum'\np1\nccopy_reg\n_reconstructor\np2\n(c__main__\nanti_pickle_serum\np3\nc__builtin__\nobject\np4\nNtp5\nRp6\ns."]
+    picklable_data = check_object_picklable(decrypted_data)
+    flag = False
+    for i in range(len(picklable_data)):
+        if picklable_data[i] != False:
+            print(url+ ":" + str(python_ports[i]) +" might be susceptible to Insecure Deserialization")
+            flag = True
+    
+    if flag == False:
+        print("This website is not susceptible to Insecure Deserialization")
